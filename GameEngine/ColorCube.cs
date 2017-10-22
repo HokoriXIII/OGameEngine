7b60af959103e6e67deff4dc4c6db737850738b5
ozeep <zeper66@gmail.com>
2017-10-23 00:49:51 +0300

    Load
    H

---
+++ GameEngine/ColorCube.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+
+namespace GameEngine
+{
+    /// <summary>
+    /// A cube with a set color
+    /// </summary>
+    class ColorCube : Cube
+    {
+        Vector3 Color = new Vector3(1, 1, 1);
+
+        public ColorCube(Vector3 color) : base()
+        {
+            Color = color;
+        }
+
+        public override Vector3[] GetColorData()
+        {
+            return new Vector3[] {
+            Color,
+            Color,
+            Color,
+            Color,
+            Color,
+            Color,
+            Color,
+            Color
+        };
+        }
+    }
+}
---
+++ GameEngine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace GameEngine
+{
+    static class Program
+    {
+
+        static void Main()
+        {
+            using (Game game = new Game())
+            {
+                
+                game.Run(60.0,60.0);
+            }
+        }
+    }
+}
